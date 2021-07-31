3a08ced7abbea7d9848b071c6ad9da22aca8e9d4
Bruna <buhh_martins@hotmail.com>
2021-07-30 23:14:00 -0300

    html teste dois

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/categoria', (req, res)=>{
+    res.sendFile(__dirname + "/views/categoria.html");
+});
+
+app.listen(3000,()=>
+console.log("Bem-Vindos a InfoDevs"));
