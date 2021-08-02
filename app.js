74934b57fd794418beaa3fea9fa70dfed83f7459
Jonatan Rocha <jonatanivanhoe@gmai.com>
2021-08-01 21:42:37 -0300

    adicionando produtos

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ app.use(express.static('public'));
 app.get('/categoria', (req, res) => {
     res.sendFile(__dirname + "/views/categoria.html");
 });
+app.get('/detalhes', (req, res) => {
+    res.sendFile(__dirname + "/views/detalhesdoproduto.html");
+});
 
 app.listen(3000, () => console.log("Bem-vindos a InfoDevs"));
