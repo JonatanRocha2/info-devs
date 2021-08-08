a031c8509165b754d1c09974d6878a4f4b916851
Jonatan Rocha <jonatanivanhoe@gmai.com>
2021-08-08 20:57:38 -0300

    ajeitando as rotas v.1

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const app = express();
-
+app.listen(3000, () => {console.log("Bem-vindos a InfoDevs")});
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//rotas
+
 app.get('/categoria', (req, res)=>{
-    res.sendFile(__dirname + "/views/categoria.html");
+    res.sendFile(__dirname + "/src/views/categoria.html");
 });
 app.get('/conectores', (req, res) => {
-    res.sendFile(__dirname + "/views/conectores.html");
+    res.sendFile(__dirname + "/src/views/conectores.html");
 });
 app.get('/smartphone', (req, res) => {
-    res.sendFile(__dirname + "/views/smartphone.html");
+    res.sendFile(__dirname + "/src/views/smartphone.html");
 });
 app.get('/computador', (req, res) => {
-    res.sendFile(__dirname + "/views/computador.html");
+    res.sendFile(__dirname + "/src/views/computador.html");
 });
 app.get('/impressora', (req, res) => {
-    res.sendFile(__dirname + "/views/impressora.html");
+    res.sendFile(__dirname + "/src/views/impressora.html");
 });
 app.get('/monitor', (req, res) => {
-    res.sendFile(__dirname + "/views/monitor.html");
+    res.sendFile(__dirname + "/src/views/monitor.html");
 });
-
-app.listen(3000, () => console.log("Bem-vindos a InfoDevs"));
